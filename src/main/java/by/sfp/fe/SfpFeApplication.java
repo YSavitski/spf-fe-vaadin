1df28fd3dd722a7b6e9c6d5504985b086f2629d2
YauheniSavitski <07ersh@gmail.com>
2018-02-13 11:43:16 +0300

    Gradle project initialization with .gitignore rules for it

---
+++ src/main/java/by/sfp/fe/SfpFeApplication.java
@@ -0,0 +1,12 @@
+package by.sfp.fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfpFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfpFeApplication.class, args);
+	}
+}
---
+++ src/test/java/by/sfp/fe/SfpFeApplicationTests.java
@@ -0,0 +1,16 @@
+package by.sfp.fe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfpFeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
